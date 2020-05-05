a79a4c83bc690f5d885536e915e8a5b2c23ad819
Sachin Gaikwad <sgaikwad@cisco.com>
2020-05-04 18:28:51 -0700

    starting commit

---
+++ src/main/java/com/codegolfer/howdoi/App.java
@@ -0,0 +1,13 @@
+package com.codegolfer.howdoi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
